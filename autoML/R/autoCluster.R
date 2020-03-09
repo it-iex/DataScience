09b742a8b97637b54f6f6ad694259a4ff2a4e83a
Brett <brett.waugh2@gmail.com>
2020-03-09 15:23:49 -0400

    Update autoCluster.R
    made into function. still in dev.

--- autoML/R/autoCluster.R
+++ autoML/R/autoCluster.R
@@ -1,101 +1,120 @@
+#' @export
+
+autoCluster_dev <- function(dat, k) {
+  ##### Create where the results will go. #####
+
+  results <- list()
+
+  ##### Method one. K-means. #####
+
+  fit_kmeans <- kmeans(dat, k)
+
+  plot_kmeans <-
+    clusplot(
+      dat,
+      fit_kmeans$cluster,
+      color = TRUE,
+      shade = TRUE,
+      labels = 2,
+      lines = 0
+    )
 
 
-##### Required libraries. #####
-library(fpc)
-library(cluster)
-library(mclust)
+  ##### Method two. K-medoids. #####
 
-##### Data. ######
-dat <- mtcars
+  fit_pam <- pam(dat, k)
 
-##### Data. ######
-dat <- mtcars
+  plot_pam <-
+    clusplot(
+      dat,
+      fit_pam$clustering,
+      color = TRUE,
+      shade = TRUE,
+      labels = 2,
+      lines = 0
+    )
 
-##### Create where the results will go. #####
+  fit_pamk <- pamk(dat, k)
 
-results <- list()
-
-##### Method one. K-means. #####
-
-fit_kmeans <- kmeans(dat, k)
-
-#clusplot(mtcars, fit_kmeans$cluster, color=TRUE, shade=TRUE, labels = 2, lines = 0)
-
-
-##### Method two. K-medoids. #####
-
-fit_pam <- pam(dat, k)
-
-#clusplot(mtcars, fit_pam$pamobject$clustering, color=TRUE, shade=TRUE, labels = 2, lines = 0)
-
-fit_pamk <- pamk(dat, k)
-
-#clusplot(mtcars, fit_pamk$pamobject$clustering, color=TRUE, shade=TRUE, labels = 2, lines = 0)
+  plot_pamk <-
+    clusplot(
+      dat,
+      fit_pamk$pamobject$clustering,
+      color = TRUE,
+      shade = TRUE,
+      labels = 2,
+      lines = 0
+    )
 
 
-##### Method two. Hierarchical Clustering #####
+  ##### Method two. Hierarchical Clustering #####
 
-# Different parameters to be tested.
-distList <-
-  c("euclidean",
-    "maximum",
-    "manhattan",
-    "canberra",
-    "binary",
-    "minkowski")
+  # Different parameters to be tested.
+  distList <-
+    c("euclidean",
+      "maximum",
+      "manhattan",
+      "canberra",
+      "binary",
+      "minkowski")
 
-modelList <-
-  c(
-    "ward.D",
-    "ward.D2",
-    "single",
-    "complete",
-    "average",
-    "mcquitty",
-    "median",
-    "centroid"
-  )
+  modelList <-
+    c(
+      "ward.D",
+      "ward.D2",
+      "single",
+      "complete",
+      "average",
+      "mcquitty",
+      "median",
+      "centroid"
+    )
 
-i <- 0
-j <- 0
+  i <- 0
+  j <- 0
 
-# Training the models.
+  # Training the models.
 
-for (mod in modelList) {
-  for (dis in distList) {
-    # Distance matrix.
-    d <- dist(dat, method = dis)
+  for (mod in modelList) {
+    for (dis in distList) {
+      # Distance matrix.
+      d <- dist(dat, method = dis)
 
-    # Trains, then sends to results.
-    results[[paste(mod, dis, sep = ".")]] <- hclust(d, method = mod)
+      # Trains, then sends to results.
+      results[[paste(mod, dis, sep = ".")]] <-
+        hclust(d, method = mod)
 
-    j <- j + 1
+      j <- j + 1
+    }
+    i <- i + 1
   }
-  i <- i + 1
+
+
+  ##### Method three. Mixture model based. #####
+  # https://towardsdatascience.com/mixture-modelling-from-scratch-in-r-5ab7bfc83eef
+
+  fit_mclust <- Mclust(dat, k)
+
+  plot_mixed <- plot(fit_mclust)
+
+  ##### Create results list. #####
+
+  results$fit.kmeans <- fit_kmeans
+
+  results$fit.pam <- fit_pam
+
+  results$fit.pamk <- fit_pamk
+
+  results$fit.mixed <- fit_mclust
+
+  ##### Plots. #####
+  results$plot.kmeans <- plot_kmeans
+
+  results$plot.pam <- plot_pam
+  results$plot.pamk <- plot_pamk
+
+  results$plot.mixed <- plot_mixed
+
+  ##### Output return. #####
+  return(results)
 }
-
-
-##### Method three. Mixture model based. #####
-# https://towardsdatascience.com/mixture-modelling-from-scratch-in-r-5ab7bfc83eef
-
-fit_mclust <- Mclust(mydata, k)
-
-# Confusion matrix.
-table(y, fit_mclust$classification)
-
-
-##### Method four. DBscan. #####
-#TODO finish this method.
-# http://www.sthda.com/english/wiki/wiki.php?id_contents=7940
-
-# Determine proper eps value.
-
-
-# Perform the calculation.
-fit_dbscan <-  fpc::dbscan(df, eps = 0.15, MinPts = 5)
-
-# Use predict.
-
-
-# Plot.
-plot(fit_dbscan, mydata, main = "DBSCAN", frame = FALSE)
